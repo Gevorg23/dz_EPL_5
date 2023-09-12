e93642f4d27d746531623c6d4f15805bacea8cc2
Gevorg23 <138781631+Gevorg23@users.noreply.github.com>
2023-09-12 19:47:30 +0700

    Add files via upload
    English Premier League

---
+++ data.js
@@ -0,0 +1,50 @@
+export const dataInfo = `[
+    {
+        "id": "1",
+        "name": "Arsenal FC",
+        "age": "136",
+        "url": "https://brandlogos.net/wp-content/uploads/2015/10/arsenal-fc-logo-768x768.png" 
+    },
+    {
+        "id": "2",
+        "name": "Manchester City",
+        "age": "143",
+        "url": "https://brandlogos.net/wp-content/uploads/2017/05/manchester-city-fc-logo-768x768.png"
+    },  
+    {
+        "id": "3",
+        "name": "Manchester United",
+        "age": "145",
+        "url": "https://brandlogos.net/wp-content/uploads/2012/12/manchester-united-logo-vector.png" 
+    },
+    {
+        "id": "4",
+        "name": "Liverpool",
+        "age": "131",
+        "url": "https://brandlogos.net/wp-content/uploads/2011/08/liverpool-logo-vector.png" 
+    },
+    {
+        "id": "5",
+        "name": "Chelsea FC",
+        "age": "118",
+        "url": "https://brandlogos.net/wp-content/uploads/2011/08/chelsea-logo-vector.png" 
+    },
+    {
+        "id": "6",
+        "name": "West Ham United",
+        "age": "128",
+        "url": "https://brandlogos.net/wp-content/uploads/2016/11/west-ham-united-fc-logo-preview.png" 
+    },
+    {
+        "id": "7",
+        "name": "Newcastle United",
+        "age": "130",
+        "url": "https://brandlogos.net/wp-content/uploads/2014/10/newcastle-united-logo-preview.png" 
+    },
+    {
+        "id": "8",
+        "name": "Tottenham Hotspur",
+        "age": "141",
+        "url": "https://brandlogos.net/wp-content/uploads/2014/10/tottenham-hotspur-fc-logo.png" 
+    }
+]`;
